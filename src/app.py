1ffa7fe3fdd0683606e3cf4a7f3d27fee0ed1619
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-10-23 18:11:09 +0000

    almost done

--- src/app.py
+++ src/app.py
@@ -1,12 +1,8 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -27,13 +23,60 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = members
-
-
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Body is required'}), 400 
+    if 'first_name' not in body:
+        return jsonify({'msg':'first_name field in body required'}), 400
+    if 'age' not in body:
+        return jsonify({'msg':'age field in body required'}), 400
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg':'lucky_numbers field in body required'}), 400
+    id_member = jackson_family._generateId()
+    if 'id_member' in body:
+        id_member = body['id_member']
+    new_member = {
+        'id': id_member,
+        'first_name': body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+    jackson_family.add_member(new_member)
+    return jsonify(new_member), 201
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({'error': 'Member not found'}), 404
+
+@app.route('/member/<int:id>', methods=['PUT'])
+def update_member(id):
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Body is required'}), 400 
+    updated_member = jackson_family.update_member(id, body)
+    if updated_member:
+        return jsonify(updated_member), 200
+    else:
+        return jsonify({'error': 'Member not found'}), 404
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({'error': 'Member not found'}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -42,17 +42,30 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return member
+        return None
+
+    def update_member(self, id, new_data):
+        for member in self._members:
+            if member["id"] == id:
+                member.update(new_data)
+                return member
+        return None
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
 
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
